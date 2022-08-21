c9304a757b33ee5414859cb361bda9dd5bbd1fe3
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-08-21 17:05:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+let employee = {name: 'kelvin', streetAddress: 'kikuyu'};
+
+function updateEmployeeWithKeyAndValue(){
+    let newEmployee = {...employee};
+    newEmployee.name = 'Sam';
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+
+function deleteFromEmployeeByKey(){
+    let newEmployee = {employee};
+    delete newEmployee.name;
+    return newEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    let newEmployee = delete employee.name;
+    return employee;
+}
